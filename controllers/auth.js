20b89de9b18f0d2a98d353d2dec242d05a5b9333
codepanther <pezechigozirim@gmail.com>
2022-04-17 07:54:17 +0100

    create user model schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
 export const register = (req, res) => {
-  res.send('you hit the register endpoint from the controller.');
+  console.log(req.body);
+  res.json('you hit the register endpoint from the controller.');
 };
---
+++ models/user.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64
+    },
+    picture: {
+      type: String,
+      default: '/avatar.png'
+    },
+    role: {
+      type: Array,
+      default: ['Subscriber'],
+      enum: ['Subscriber', 'Instructor', 'Admin']
+    },
+    stripe_account_id: '',
+    stripe_seller: {},
+    stripeSession: {}
+  },
+  { timeStamps: true }
+);
+
+export default mongoose.model('User', userSchema);
