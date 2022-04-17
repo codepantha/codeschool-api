8bd617e65c04f290b468e9312f5001106e5a523b
codepanther <pezechigozirim@gmail.com>
2022-04-17 06:55:51 +0100

    set up mongodb connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 const morgan = require('morgan');
+import mongoose from 'mongoose';
 require('dotenv').config();
 import { readdirSync } from 'fs';
 
 // create express app
 const app = express();
+
+// set up database
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const dbConn = mongoose.connection;
+
+dbConn.on('error', () => console.log("Database connection error."));
+dbConn.on('connected', () => console.log("Database connected."));
 
 // apply middleware
 app.use(cors());
